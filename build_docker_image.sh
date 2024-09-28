2a75ddd65b50ca970c08a4fe2917f4c2c68af28d
Dan Gutt <dgutt10@gmail.com>
2024-09-27 23:13:12 -0500

    rbenv

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker build -t my-linux-image -f linux/Dockerfile .
-docker build -t my-ruby-image -f ruby/Dockerfile .
-#docker build -t my-rails-image rails/Dockerfile
+docker build -t my-ruby-image -f docker/ruby/Dockerfile .
+docker build -t my-rails-image -f docker/rails/Dockerfile .
